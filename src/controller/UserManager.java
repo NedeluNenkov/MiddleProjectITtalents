d0bee02ffec28cede801f09067c66d2e4a7741e4
[NikolayTolev] <[nikolaytolev93@gmail.com]>
2018-04-01 16:02:30 +0300

    refactoring the code

---
+++ src/controller/UserManager.java
@@ -0,0 +1,138 @@
+package controller;
+
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+import model.Comment;
+import model.Post;
+import model.User;
+import model.UserDAO;
+
+public class UserManager {
+
+	private static volatile UserManager instance;
+	private HashSet<User> loggedUsers;
+	
+	private UserManager() {
+		this.loggedUsers = new HashSet<>();
+	}
+
+	public static UserManager getInstance() {
+		if (instance == null) {
+			synchronized (UserManager.class) {
+				if (instance == null) {
+					instance = new UserManager();
+				}
+			}
+		}
+		return instance;
+	}
+
+	public void login(String username, String password) {
+		boolean exists;
+		try {
+			exists = UserDAO.getInstance().checkForUser(username, password);
+			if (!exists) {
+				throw new IllegalArgumentException("Incorrect username/password!");
+			} else {
+				this.loggedUsers.add(UserDAO.getInstance().getByUsername(username));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void logout(User u) {
+		this.loggedUsers.remove(u);
+	}
+
+	public void register(String username, String firstName, String lastName, String password, String email,
+			String passwordCopy) {
+		if (!password.equals(passwordCopy)) {
+			throw new IllegalArgumentException("Passwords do not match!");
+		}
+		try {
+			for (User user : UserDAO.getInstance().getAllUsers()) {
+				if (user.getUsername() == username || user.getEmail() == email) {
+					throw new IllegalArgumentException("User with this sername/email already exists");
+				}
+			}
+			User u = new User(username, firstName, lastName, passwordCopy, email);
+			UserDAO.getInstance().saveUser(u);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+//	public void createFolder(String folderName) {
+//		if(folderName == null || folderName.isEmpty()) {
+//			throw new IllegalArgumentException("Invalid name for folder");
+//		}
+//		user.folders.put(folderName, new TreeSet<>());
+//	}
+	
+//	public void addPost(String folderName, String description) {
+//		//TODO path requires the path to the photo/video the User chooses to upload
+//		if(!user.getIsLogged()) {
+//			throw new IllegalAccessError("You must be logged in!");
+//		}
+//		Post post = new Post("here we should write the path", user);
+//		post.addDescription(description);
+//		if(folderName == null) {
+//			user.folders.get("Untiteled").add(post);
+//		} else {
+//			user.folders.get(folderName).add(post);
+//		}
+//		user.posts.add(post);
+//	}
+//		
+//	public void writeComment(Post post) {
+//		if(this.loggedUsers.contains(arg0)) {
+//			throw new IllegalAccessError("You must be logged in!");
+//		}
+//		// read Text
+//		Scanner sc = new Scanner(System.in);
+//		String text = sc.nextLine();
+//		post.addComment(new Comment(user, text));
+//		sc.close();
+//	}
+	
+//	public User search(String username) {
+//		if (username == null || username.isEmpty()) {
+//			throw new IllegalArgumentException("No match found");
+//			return ;
+//		}
+//		UserDAO.getInstance().getByUsername(username);
+//		for (int i = 0; i < users.size(); i++) {
+//			if(users.get(i).getUsername().equals(username)) {
+//				hasUser = true;
+//				userId = i;
+//				break;
+//			}
+//		}
+//		if(hasUser) {
+//			return users.get(userId);
+//		} else {
+//			throw new IllegalArgumentException("User not found!");
+//		}
+//	}
+	
+//	public void likePost(int id, Post post) {
+//		if(id == 0) {
+//			throw new IllegalAccessError("You must be logged in!");
+//		}
+//		post.likePost(user);
+//	}
+//	
+//	public void dislikePost(int id, Post post) {
+//		if(id == 0) {
+//			throw new IllegalAccessError("You must be logged in!");
+//		}
+//		post.dislikePost(user);
+//	}
+}
---
+++ src/model/UserDAO.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashSet;
+
+import model.User;
+
+public class UserDAO implements IUserDAO{
+	
+	private static volatile UserDAO userDao;
+	private Connection connection;
+	
+	private UserDAO() {
+		this.connection = DBManager.getInstance().getConnection();
+	}
+	
+	public static UserDAO getInstance() {
+		if(userDao == null) {
+			synchronized (UserDAO.class) {
+				if(userDao == null) {
+					userDao = new UserDAO();
+				}
+			}
+		}
+		return userDao;
+	}
+
+	@Override
+	public User getByID(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveUser(User u) throws Exception {
+		// using PreparedStatement we escape SQLInjection !
+				PreparedStatement s = connection.prepareStatement("INSERT INTO users (first_name, last_name, username, password, email) VALUES (?,?,?,?,?)");
+				// we set the values in the order from the query
+				s.setString(1, u.getFirstName());
+				s.setString(2, u.getLastName());
+				s.setString(3, u.getUsername());
+				s.setString(4, u.getPassword());
+				s.setString(5, u.getEmail());
+				s.executeUpdate();
+		
+	}
+
+	@Override
+	public void changeUser(User u) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<User> getAllPosts() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getByUsername(String username) throws Exception {
+		return null;
+	}
+
+	@Override
+	public boolean checkForUser(String username, String password) throws Exception {
+		for (User u : getAllUsers()) {
+			if (username == u.getUsername() && password == u.getPassword()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public Collection<User> getAllUsers() throws SQLException {
+		PreparedStatement s = connection.prepareStatement("SELECT username FROM users");
+		HashSet<User> users = new HashSet<>();
+		ResultSet result = s.executeQuery();
+		while(result.next()) {
+			User u = new User(	result.getInt("id"),
+								result.getString("fristName"), 
+								result.getString("lastName"),
+								result.getString("username"), 
+								result.getString("email"),
+								result.getString("pasword"));
+			users.add(u);
+		}
+		return users;
+	}
+	
+	
+}
